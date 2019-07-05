f17d1ef28691cc6a7931a0326039f3d07b3b16c1
mona-n67 <mona.nasrolahi@yahoo.com>
2019-07-05 22:55:08 +0430

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+ function sum(){
+    var c = (b1.value*1) + (b2.value*1);
+    output.innerHTML = c ;
+ }
+
+ function sub(){
+    var c = (s1.value*1) - (s2.value*1);
+    output.innerHTML = c ;
+}
+
+function multi(){
+    var c = (m1.value*1) * (m2.value*1);
+    output.innerHTML = c ;
+}
+
+function div(){
+    var c = (d1.value*1) / (d2.value*1);
+    output.innerHTML = c ;
+}
+
+
+
+ 
+ 
+ 
+ 
